38c3c90eaa3949f85f86d3a9bf77d12f1dcea324
EkawaD <steo.ederhy@gmail.com>
2022-06-03 16:26:32 +0200

    destroy_infra

--- destroy_infra.sh
+++ destroy_infra.sh
@@ -3,3 +3,8 @@
 # Récupérer le client-id pour faire un terraform destroy
 # depuis le bon dossier.
 
+bash ./GENERATORS/check_env.sh
+echo "Finished check env"
+cd /var/jenkins-home/archi/${CLIENT_ID}
+terraform destroy
+echo "Terraform destroyed for client : {$CLIENT_ID}"
